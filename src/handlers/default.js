e61b33173ca8a72fc08ee06b5019c8fe4661f61d
Nicolo Maioli <nmaioli@and.digital>
2019-10-13 21:12:43 +0100

    Remove no need to check if connection exists

--- src/handlers/default.js
+++ src/handlers/default.js
@@ -2,47 +2,9 @@ const AWS = require('aws-sdk')
 const { sendReply } = require('../utils/sendReply')
 
 exports.handler = async (event, _context) => {
-  // Verify that connection exists in the database
   const connectionId = event.requestContext.connectionId
   const CONNECTION_TABLE = process.env.CONNECTION_TABLE
   const dynamoDbClient = new AWS.DynamoDB()
-
-  const queryParams = {
-    TableName: CONNECTION_TABLE,
-    KeyConditionExpression: 'connectionId = :cId',
-    ExpressionAttributeValues: {
-      ':cId': {
-        S: connectionId
-      }
-    }
-  }
-
-  const connectionExists = await dynamoDbClient
-    .query(queryParams)
-    .promise()
-    .then(data => {
-      if (!data.Items.length) {
-        return false
-      }
-
-      return true
-    })
-    .catch(err => {
-      console.error(err.message)
-
-      return false
-    })
-
-  if (!connectionExists) {
-    console.log(`Connection id ${connectionId} not found in ${CONNECTION_TABLE}`)
-
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        error: `Connection not found with id ${event.requestContext.connectionId}`
-      })
-    }
-  }
 
   // Send a response
   const domainName = event.requestContext.domainName
