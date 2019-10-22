13b23645d5f4b341099c1fd09ddbc24d77cba4bc
Nicolo Maioli <nmaioli@and.digital>
2019-10-22 18:14:54 +0200

    Connect handler 100% unit test coverage

--- __tests__/connect.test.js
+++ __tests__/connect.test.js
@@ -27,8 +27,51 @@ describe('$connect', () => {
       }
     }
 
+    const expectedParams = {
+      TableName: 'test',
+      Item: {
+        connectionId: {
+          S: 'test'
+        }
+      }
+    }
+
     handler(event)
     expect(putItemSpy.calledOnce).toBeTruthy()
+    expect(putItemSpy.calledWith(expectedParams)).toBeTruthy()
+    done()
+  })
+
+  test('It returns 200 if the connection is saved', async done => {
+    AWSMock.mock('DynamoDB', 'putItem', (_params, callback) => {
+      callback(null, 'success')
+    })
+
+    const event = {
+      requestContext: {
+        connectionId: 'test'
+      }
+    }
+
+    const response = await handler(event)
+    expect(response.statusCode).toBe(200)
+    done()
+  })
+
+  test('It returns 500 if the connection is not saved', async done => {
+    AWSMock.mock('DynamoDB', 'putItem', (_params, callback) => {
+      const error = new Error('test error')
+      callback(error)
+    })
+
+    const event = {
+      requestContext: {
+        connectionId: 'test'
+      }
+    }
+
+    const response = await handler(event)
+    expect(response.statusCode).toBe(500)
     done()
   })
 })
