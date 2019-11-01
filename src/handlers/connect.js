afe4608d3d78df5546dbfefcbb2a9e26da5ad4ad
Nicolo Maioli <nmaioli@and.digital>
2019-11-01 10:59:33 +0000

    Message all clients on connection

--- src/handlers/connect.js
+++ src/handlers/connect.js
@@ -1,23 +1,42 @@
 'use strict'
 
 const AWS = require('aws-sdk')
+const getRandomUsername = require('../utils/getRandomUsername')
+const getAllConnections = require('../utils/getAllConnections')
+const { sendMany } = require('../utils/sendMessage')
 
 exports.handler = async (event, _context) => {
   const CONNECTION_TABLE = process.env.CONNECTION_TABLE
   const connectionId = event.requestContext.connectionId
+  const username = getRandomUsername()
 
-  const dynamoDbClient = new AWS.DynamoDB()
+  const db = new AWS.DynamoDB()
 
+  // Get all connected clients
+  const connections = await getAllConnections(db, CONNECTION_TABLE)
+    .then(data => {
+      console.log(data)
+      return data
+    })
+    .catch(err => {
+      console.error(err)
+      return []
+    })
+
+  // Record new connection
   const putParams = {
     TableName: CONNECTION_TABLE,
     Item: {
       connectionId: {
         S: connectionId
+      },
+      username: {
+        S: username
       }
     }
   }
 
-  await dynamoDbClient
+  await db
     .putItem(putParams)
     .promise()
     .then(data => {
@@ -29,6 +48,25 @@ exports.handler = async (event, _context) => {
       console.error(err)
       throw err
     })
+
+  // Inform connected clients of new user
+  const { domainName, stage } = event.requestContext
+  const timestamp = new Date()
+  const postData = JSON.stringify({
+    username,
+    action: 'CONNECTED',
+    timestamp: timestamp.toISOString(),
+    message: ''
+  })
+
+  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+    endpoint: `${domainName}/${stage}`
+  })
+
+  if (connections.length) {
+    await sendMany(apigwManagementApi, postData, connections, db, CONNECTION_TABLE)
+      .catch(err => console.error(err))
+  }
 
   return {
     statusCode: 200,
